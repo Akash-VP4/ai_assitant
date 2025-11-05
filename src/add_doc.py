0cd52468f24945654b03d5cb8867160932187c88
Akash VP <akash@bititude.com>
2025-11-05 17:19:07 +0530

    add: python-version,project.toml,uv.lock

---
+++ src/add_doc.py
@@ -0,0 +1,33 @@
+ 
+from typing import List, Any
+import numpy as np
+ 
+
+def add_document(document: list[Any], embeddings: np.array,collection):
+    """This will add document to vector db
+    Args:
+        document:
+        embeddings:
+        collection:
+    """
+
+    if len(document['ids']) != len(embeddings):
+        raise ValueError(
+            f"Number of document({len(document['ids'])}) must match number of embedding({len(embeddings)})"
+        )
+
+    try:
+        # print(document["documents"])
+        collection.add(
+            ids=document["ids"],
+            documents=document["documents"],
+            embeddings=embeddings,
+            metadatas=document["metadatas"],
+        )
+        print(f"[DEGUB]: Added {collection.count()} document to {collection}")
+
+    except Exception as e:
+        print(f"[ERORR]: Failed to add documents: {e}")
+
+def print_something():
+  print("heyy")
